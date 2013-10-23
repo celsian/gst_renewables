0b7de06f6d4000bd9ee6b8b4d7d369e4d38d6ced
Celsian <celsian@gmail.com>
2013-10-23 15:54:15 -0700

    Added resource routes for PV Commissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ GstRenewables::Application.routes.draw do
   devise_for :users
   root to: "projects#index"
   resources :projects
+  resources :pv_commissions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
