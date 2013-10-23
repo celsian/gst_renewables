f8a7b8fe19a8c060d57e85a8fde9e6300926f34b
Celsian <celsian@gmail.com>
2013-10-23 09:54:37 -0700

    Standard CRUD for Projects
    Added strong params as well

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,49 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      # Note.create(student: @student, note: "Student #{@student.id_number} was created.")
+      redirect_to projects_path, flash: {success: "Project was created."}
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    Project.find(params[:id]).destroy
+    redirect_to projects_path, flash: {success: "Project was deleted."}
+  end
+
+  def update
+    project = Project.find(params[:id])
+    project.attributes = project_params
+    if project.save
+      redirect_to project, flash: {success: "Project was updated."}
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
