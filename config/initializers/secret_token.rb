93d0642fcbad23268f45e225d0777ca3c89624af
Celsian <celsian@gmail.com>
2013-10-23 08:47:54 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GstRenewables::Application.config.secret_key_base = '12ffd9b0c995a3082140b0be71eeac0eda23d722ed575c4d815477502d7d3c42ab6619cc6caec4d7ae407399b5d94d032170a779a25fc80dc523efb88df08de9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GstRenewables::Application.config.session_store :cookie_store, key: '_gst_renewables_session'
