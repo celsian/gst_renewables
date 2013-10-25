426ca0f24732337c3f874acf1e19be13e0b47f23
Celsian <celsian@gmail.com>
2013-10-25 12:11:45 -0700

    Adding mount_uploaders to PvArrayTest combiner pics

--- app/models/pv_array_test.rb
+++ app/models/pv_array_test.rb
@@ -1,4 +1,8 @@
 class PvArrayTest < ActiveRecord::Base
   belongs_to :pv_commission
+
+  mount_uploader :combiner_exterior_pic, PicUploader
+  mount_uploader :combiner_interior_pic, PicUploader
+  mount_uploader :combiner_flir_pic, PicUploader
   
 end
