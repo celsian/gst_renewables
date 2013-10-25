b434b161506a6e32ffc96b4d6e7b6ed28a491e46
Celsian <celsian@gmail.com>
2013-10-25 12:15:25 -0700

    t.reference -> t.references

--- db/migrate/20131025005815_create_pv_array_tests.rb
+++ db/migrate/20131025005815_create_pv_array_tests.rb
@@ -1,7 +1,7 @@
 class CreatePvArrayTests < ActiveRecord::Migration
   def change
     create_table :pv_array_tests do |t|
-      t.reference :pv_commission
+      t.references :pv_commission, index: true
       t.string :combiner_exterior_pic
       t.string :combiner_interior_pic
       t.string :combiner_flir_pic
