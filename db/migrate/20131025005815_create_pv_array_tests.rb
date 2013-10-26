2df9b80b936fbc4598f1cfee0187d1d730ed3776
Celsian <celsian@gmail.com>
2013-10-26 14:50:56 -0700

    Additional columns per the clients request

--- db/migrate/20131025005815_create_pv_array_tests.rb
+++ db/migrate/20131025005815_create_pv_array_tests.rb
@@ -2,6 +2,12 @@ class CreatePvArrayTests < ActiveRecord::Migration
   def change
     create_table :pv_array_tests do |t|
       t.references :pv_commission, index: true
+      t.string :test_number
+      t.boolean :initial_verification
+      t.string :inspector
+      t.string :test_instruments
+      t.string :combiner
+      t.string :mfg
       t.string :combiner_exterior_pic
       t.string :combiner_interior_pic
       t.string :combiner_flir_pic
