b09ffb539d5077f8a2e81d9d978605d51078fa90
Celsian <celsian@gmail.com>
2013-10-28 10:46:43 -0700

    Additional thumb nail processing for string_test pictures

--- app/uploaders/pic_uploader.rb
+++ app/uploaders/pic_uploader.rb
@@ -35,7 +35,9 @@ class PicUploader < CarrierWave::Uploader::Base
   version :thumb do
     process :resize_to_limit => [200, 200]
   end
-
+  version :thumb_small do
+    process :resize_to_limit => [50, 50]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
