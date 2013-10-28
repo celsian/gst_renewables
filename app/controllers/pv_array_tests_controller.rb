90068e12e82336c4cceee2e0800a6a03d34fbc82
Celsian <celsian@gmail.com>
2013-10-28 15:15:18 -0700

    Fixed a silly boolean mistake = -> ==

--- app/controllers/pv_array_tests_controller.rb
+++ app/controllers/pv_array_tests_controller.rb
@@ -14,10 +14,10 @@ class PvArrayTestsController < ApplicationController
   def create
     pv_array_test = PvArrayTest.new(pv_array_test_params)
 
-    if pv_array_test.pv_commission.pv_array_tests = []
+    if pv_array_test.pv_commission.pv_array_tests == []
       pv_array_test.test_number = "001"
     else
-      pv_array_test.test_number = (pv_array_test.pv_commission.pv_array_tests.last.test_number.to_i+1).to_s.rjust(3, '0')
+      pv_array_test.test_number = ((((pv_array_test.pv_commission.pv_array_tests.last).test_number).to_i)+1).to_s.rjust(3, '0')
     end
 
     if pv_array_test.save
