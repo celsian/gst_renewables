b2cff4f2cc7a26123f3dce97e9c72acc624795d6
Celsian <celsian@gmail.com>
2013-10-28 01:05:47 -0700

    Rails Adds

--- app/models/pv_array_test.rb
+++ app/models/pv_array_test.rb
@@ -1,5 +1,6 @@
 class PvArrayTest < ActiveRecord::Base
   belongs_to :pv_commission
+  has_many :string_tests
 
   mount_uploader :combiner_exterior_pic, PicUploader
   mount_uploader :combiner_interior_pic, PicUploader
