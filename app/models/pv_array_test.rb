50206ca0c9f2067e8ed5c57e4461659546a583f3
Celsian <celsian@gmail.com>
2013-10-29 12:16:48 -0700

    Renamed carrierwave upload to CombinerUploader to avoid multiple un-necessary thumbnails

--- app/models/pv_array_test.rb
+++ app/models/pv_array_test.rb
@@ -2,8 +2,8 @@ class PvArrayTest < ActiveRecord::Base
   belongs_to :pv_commission
   has_many :string_tests
 
-  mount_uploader :combiner_exterior_pic, PicUploader
-  mount_uploader :combiner_interior_pic, PicUploader
-  mount_uploader :combiner_flir_pic, PicUploader
+  mount_uploader :combiner_exterior_pic, CombinerUploader
+  mount_uploader :combiner_interior_pic, CombinerUploader
+  mount_uploader :combiner_flir_pic, CombinerUploader
   
 end
