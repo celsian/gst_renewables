4acf0caa47d1993369b4d8b2d44aa470fc9aa516
Celsian <celsian@gmail.com>
2013-10-31 15:56:10 -0700

    Rails Adds

---
+++ app/controllers/string_tests_controller.rb
@@ -0,0 +1,16 @@
+class StringTestsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit_individual
+  end
+
+  def update_individual
+  end
+end
---
+++ app/helpers/string_tests_helper.rb
@@ -0,0 +1,2 @@
+module StringTestsHelper
+end
