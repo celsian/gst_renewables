5bf2430bcfaff8a0df8652a4ce847601025ac2e6
Celsian <celsian@gmail.com>
2013-11-02 18:37:27 -0700

    Added default scope and nested attributes for :string_tests

--- app/models/pv_array_test.rb
+++ app/models/pv_array_test.rb
@@ -2,6 +2,10 @@ class PvArrayTest < ActiveRecord::Base
   belongs_to :pv_commission
   has_many :string_tests
 
+  default_scope order("id")
+
+  accepts_nested_attributes_for :string_tests
+
   mount_uploader :combiner_exterior_pic, CombinerUploader
   mount_uploader :combiner_interior_pic, CombinerUploader
   mount_uploader :combiner_flir_pic, CombinerUploader
