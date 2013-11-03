d5b0187d9c279a4c952feb1af4f40ce6b9f0d669
Celsian <celsian@gmail.com>
2013-11-02 18:41:30 -0700

    Updated migration for string_test for voltage and string name

--- db/migrate/20131027210127_create_string_tests.rb
+++ db/migrate/20131027210127_create_string_tests.rb
@@ -1,6 +1,8 @@
 class CreateStringTests < ActiveRecord::Migration
   def change
     create_table :string_tests do |t|
+      t.references :pv_array_test, index: true
+      t.string :name
       t.string :array_module
       t.string :array_quantity
       t.string :array_parameters_voc
@@ -15,6 +17,7 @@ class CreateStringTests < ActiveRecord::Migration
       t.string :string_test_voc
       t.string :string_test_isc
       t.string :string_test_irradiance
+      t.string :string_test_voltage
       t.string :array_insulation_resistance_test_voltage
       t.string :array_insulation_resistance_pos
       t.string :array_insulation_resistance_neg
