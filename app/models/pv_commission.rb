0ad01657525a5b43195a316c385f874db503b5a1
Celsian <celsian@gmail.com>
2014-02-20 08:21:56 -0800

    New function for pv_commission model to count rows for the show view.

--- app/models/pv_commission.rb
+++ app/models/pv_commission.rb
@@ -3,4 +3,8 @@ class PvCommission < ActiveRecord::Base
   has_many :visual_inspections
   has_many :pv_array_tests
   has_many :inverter_inspections
+
+  def row_count
+    [pv_array_tests.length, visual_inspections.length, inverter_inspections.length].max
+  end
 end
