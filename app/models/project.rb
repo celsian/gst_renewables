f869918852fd888dc5f9dd503ea954097f2a04f2
Celsian <celsian@gmail.com>
2014-02-23 20:10:10 -0800

    Fixed depreciation and added validation for reference_number

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   has_many :pv_commissions
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :reference_number, uniqueness: true
 
-  default_scope order("name")
+  default_scope { order("name") }
 end
