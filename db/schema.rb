346a5f9fe7bf09c2667e349543759df2ba411e2e
Celsian <celsian@gmail.com>
2014-02-23 20:16:16 -0800

    Schema changes for reference numbers.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216220056) do
+ActiveRecord::Schema.define(version: 20140222210233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140216220056) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "reference_number"
   end
 
   create_table "pv_array_tests", force: true do |t|
@@ -116,8 +117,8 @@ ActiveRecord::Schema.define(version: 20140216220056) do
   add_index "string_tests", ["pv_array_test_id"], name: "index_string_tests_on_pv_array_test_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -128,6 +129,9 @@ ActiveRecord::Schema.define(version: 20140216220056) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false
+    t.boolean  "editor",                 default: false
+    t.string   "reference_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
