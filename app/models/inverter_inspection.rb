9fa313d541dd5eb49c8d1d51bacf5e27abd40c8b
Celsian <celsian@gmail.com>
2014-02-28 11:00:26 -0800

    Updated for ruby 1.9

--- app/models/inverter_inspection.rb
+++ app/models/inverter_inspection.rb
@@ -1,6 +1,6 @@
 class InverterInspection < ActiveRecord::Base
   belongs_to :pv_commission
-  has_many :infoboxes
+  has_many :infoboxes, dependent: :destroy
   has_many :inspection_images, through: :infoboxes
 
   accepts_nested_attributes_for :infoboxes, :allow_destroy => true
