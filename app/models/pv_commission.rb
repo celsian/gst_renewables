f7e3eb80421eb8593c30e15c136ea915b1d0fecd
Celsian <celsian@gmail.com>
2014-02-28 11:00:41 -0800

    Added Validations

--- app/models/pv_commission.rb
+++ app/models/pv_commission.rb
@@ -4,6 +4,9 @@ class PvCommission < ActiveRecord::Base
   has_many :pv_array_tests
   has_many :inverter_inspections
 
+  validates :reference_number, presence: true
+  validates :reference_number, uniqueness: true
+
   def row_count
     [pv_array_tests.length, visual_inspections.length, inverter_inspections.length].max
   end
