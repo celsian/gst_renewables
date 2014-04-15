4c15a022eb5c55420eb3e3a11e0deae20aa4a18a
Lucas Paolella <celsian@gmail.com>
2014-04-14 17:26:36 -0700

    Added relation for recombiner, added validations to ensure recombined presence is accounted for, added row_count functionality for recombined

--- app/models/pv_commission.rb
+++ app/models/pv_commission.rb
@@ -1,13 +1,20 @@
 class PvCommission < ActiveRecord::Base
   belongs_to :project
   has_many :visual_inspections
+  has_many :recombiners
   has_many :pv_array_tests
   has_many :inverter_inspections
 
-  validates :reference_number, presence: true
+  validates :reference_number, :recombiner_presence, :project_id, presence: true
   validates :reference_number, uniqueness: true
+  
+  YESNO = ["Yes", "No"]
 
   def row_count
-    [pv_array_tests.length, visual_inspections.length, inverter_inspections.length].max
+    if recombiner_presence == "No"
+      [pv_array_tests.length, visual_inspections.length, inverter_inspections.length].max
+    else
+      [recombiners.length, visual_inspections.length, inverter_inspections.length].max
+    end
   end
 end
