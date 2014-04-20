9f3a15cfb5237ff8b005cef9b767bdea6a13db53
Lucas Paolella <celsian@gmail.com>
2014-04-20 14:16:29 -0700

    Added DAS Model

---
+++ app/models/data_acquisition_system.rb
@@ -0,0 +1,10 @@
+class DataAcquisitionSystem < ActiveRecord::Base
+  belongs_to :pv_commission
+  has_many :infoboxes, dependent: :destroy
+  has_many :inspection_images, through: :infoboxes
+
+  accepts_nested_attributes_for :infoboxes, :allow_destroy => true
+
+
+  validates :name, presence: true
+end
