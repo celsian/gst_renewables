c7dd5ebf2619b1885f75aa20dd279ce0ed4f46b9
Lucas Paolella <celsian@gmail.com>
2014-04-20 14:17:05 -0700

    name presence validation

--- app/models/inverter_inspection.rb
+++ app/models/inverter_inspection.rb
@@ -4,4 +4,7 @@ class InverterInspection < ActiveRecord::Base
   has_many :inspection_images, through: :infoboxes
 
   accepts_nested_attributes_for :infoboxes, :allow_destroy => true
+
+
+  validates :name, presence: true
 end
