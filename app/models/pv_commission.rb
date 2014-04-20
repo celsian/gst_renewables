0d0e11211c8ae6dba64d50ed46777706e51937f6
Lucas Paolella <celsian@gmail.com>
2014-04-20 14:17:53 -0700

    updated the way relations work and added DAS

--- app/models/pv_commission.rb
+++ app/models/pv_commission.rb
@@ -1,9 +1,10 @@
 class PvCommission < ActiveRecord::Base
   belongs_to :project
-  has_many :visual_inspections
-  has_many :recombiners
-  has_many :pv_array_tests
-  has_many :inverter_inspections
+  has_many :visual_inspections, dependent: :destroy
+  has_many :recombiners, dependent: :destroy
+  has_many :pv_array_tests, dependent: :destroy
+  has_many :inverter_inspections, dependent: :destroy
+  has_many :data_acquisition_systems, dependent: :destroy
 
   validates :reference_number, :recombiner_presence, :project_id, presence: true
   validates :reference_number, uniqueness: true
@@ -12,9 +13,18 @@ class PvCommission < ActiveRecord::Base
 
   def row_count
     if recombiner_presence == "No"
-      [pv_array_tests.length, visual_inspections.length, inverter_inspections.length].max
+      [pv_array_tests.length, visual_inspections.length, inverter_inspections.length, data_acquisition_systems.length].max
     else
-      [recombiners.length, visual_inspections.length, inverter_inspections.length].max
+      [recombiners.length, visual_inspections.length, inverter_inspections.length, data_acquisition_systems.length].max
     end
   end
+
+  def error_messages
+    messages = ""
+    errors.full_messages.each do |message|
+      messages += message + "."
+    end
+    messages
+  end
+  
 end
