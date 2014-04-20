3c63df04a79f131ec267c63e741c63c46676b9cc
Lucas Paolella <celsian@gmail.com>
2014-04-20 14:18:14 -0700

    added image uploader and custom validation

--- app/models/recombiner.rb
+++ app/models/recombiner.rb
@@ -5,17 +5,31 @@ class Recombiner < ActiveRecord::Base
   validates :name, :pv_commission_id, presence: true
   validate :name_uniqueness_for_pv_commission
 
-  default_scope order("name")
+  default_scope { order("name") }
+
+  mount_uploader :recombiner_exterior_pic, CombinerUploader
+  mount_uploader :recombiner_interior_pic, CombinerUploader
+  mount_uploader :recombiner_flir_pic, CombinerUploader
 
   def name_uniqueness_for_pv_commission
     names = []
     PvCommission.find(pv_commission_id).recombiners.each do |recombiner|
-      names << recombiner.name
+      unless id == recombiner.id
+        names << recombiner.name
+      end
     end
 
     if names.include?(name)
-      errors.add(:base, 'Recombiners must have unique names within a PV Commission.')
+      errors.add(:base, 'Recombiners must have unique names within a PV Commission')
     end
+  end
+
+  def error_messages
+    messages = ""
+    errors.full_messages.each do |message|
+      messages += message + "."
+    end
+    messages
   end
 
 end
