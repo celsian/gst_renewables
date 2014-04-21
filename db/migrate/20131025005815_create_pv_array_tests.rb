67fa5f5b7f23cddd29c7f52fef622f10e0a12b60
Lucas Paolella <celsian@gmail.com>
2014-04-21 06:56:36 -0700

    migrations for recombined info and inverter names

--- db/migrate/20131025005815_create_pv_array_tests.rb
+++ db/migrate/20131025005815_create_pv_array_tests.rb
@@ -2,6 +2,14 @@ class CreatePvArrayTests < ActiveRecord::Migration
   def change
     create_table :pv_array_tests do |t|
       t.references :pv_commission, index: true
+      t.references :recombiner, index: true
+      t.string :name
+      t.string :megger_test
+      t.string :comments
+      t.string :voltage
+      t.string :test_voltage
+      t.string :pos
+      t.string :neg
       t.string :test_number
       t.boolean :initial_verification
       t.string :inspector
--- db/migrate/20140216220056_create_inverter_inspections.rb
+++ db/migrate/20140216220056_create_inverter_inspections.rb
@@ -2,6 +2,7 @@ class CreateInverterInspections < ActiveRecord::Migration
   def change
     create_table :inverter_inspections do |t|
       t.references :pv_commission, index: true
+      t.string :name
 
       t.timestamps
     end
