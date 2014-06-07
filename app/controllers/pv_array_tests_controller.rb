ee52d1aadb2bde0481a5a2f5843680f55b839f6e
Lucas Paolella <celsian@gmail.com>
2014-06-06 18:11:32 -0700

    User association/disassociation to projects

--- app/controllers/pv_array_tests_controller.rb
+++ app/controllers/pv_array_tests_controller.rb
@@ -27,25 +27,30 @@ class PvArrayTestsController < ApplicationController
   def create
     pv_array_test = PvArrayTest.new(pv_array_test_create_params)
 
-    # if pv_array_test.pv_commission.pv_array_tests == []
-    #   pv_array_test.test_number = "001"
-    # else
-    #   pv_array_test.test_number = (pv_array_test.pv_commission.pv_array_tests.last.test_number.to_i+1).to_s.rjust(3, '0')
-    # end
-
     if pv_array_test.save
       if params["pv_array_test"]["string_tests_attributes"]
         StringTest.import(params["pv_array_test"]["string_tests_attributes"]["0"]["pv_array_test"], pv_array_test) #this passes the single CSV that the user selected which will always be in position 0 because there is only one string_tests.build in the pv_array_test controller
-        redirect_to pv_array_test, flash: {success: "PV Array Test was created."}
+        if pv_array_test.string_tests.size == 0
+          if pv_array_test.pv_commission
+            redirect_target = pv_array_test.pv_commission
+          else
+            redirect_target = pv_array_test.recombiner
+          end
+
+          pv_array_test.destroy
+          redirect_to redirect_target, flash: {error: "Error: PV Array Test was NOT created, String Test data for this combiner does not exist. Please check to ensure that the test data for the combiner you are adding has been included in the CSV file."}
+        else
+          redirect_to pv_array_test, flash: {success: "PV Array Test: #{pv_array_test.name} was created."}
+        end
       else
         if pv_array_test.pv_commission
-          pv_commission = pv_array_test.pv_commission
+          redirect_target = pv_array_test.pv_commission
         else
-          pv_commission = pv_array_test.recombiner.pv_commission
+          redirect_target = pv_array_test.recombiner
         end
 
         pv_array_test.destroy
-        redirect_to pv_commission, flash: {error: "Error: PV Array Test was NOT created, String Test data was not included."}
+        redirect_to redirect_target, flash: {error: "Error: PV Array Test was NOT created, String Test data was not included."}
       end
     else
       flash[:error] = "Error: #{@pv_array_test.error_messages}"
@@ -123,7 +128,7 @@ class PvArrayTestsController < ApplicationController
       project = PvArrayTest.find(params[:id]).recombiner.pv_commission.project
     end
 
-    unless project.reference_number == current_user.reference_number || current_user.admin || current_user.editor
+    unless project.users.exists?(current_user) || current_user.admin || current_user.editor
       redirect_to root_path, flash: { error: "You are not authorized to perform that action." }
     end
   end
