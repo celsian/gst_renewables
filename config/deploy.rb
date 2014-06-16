0e03677e17eaa2cc022b8c010aa8089d838dd5f8
Lucas Paolella <celsian@gmail.com>
2014-06-16 08:53:28 -0700

    Added rake db:migrate for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,7 +80,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        # execute :rake, 'cache:clear'
+        execute "RAILS_ENV=production bundle exec rake db:migrate"
       end
     end
   end
