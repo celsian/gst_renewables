a60f1d23f2cd53344a9d9569a4569bffba6aa1da
Lucas Paolella <celsian@gmail.com>
2014-06-16 08:48:18 -0700

    Remove cache clear

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,7 +80,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'cache:clear'
+        # execute :rake, 'cache:clear'
       end
     end
   end
